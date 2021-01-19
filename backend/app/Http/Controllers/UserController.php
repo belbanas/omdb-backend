8c115a2d3e9aeb7c51721107feb0da0ab33152f0
belbanas <belbanas@gmail.com>
2021-01-19 12:40:00 +0100

    add login registration

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+
+
+class UserController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function register(Request $request)
+    {
+        // regisztrál usert
+        try {
+            $this->validate($request, [
+                'name' => 'required|min:4',
+                'email' => 'required|email',
+                'password' => 'required|min:8',
+            ]);
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+
+            return response()->json(['name' => 'sd'], 200);
+
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return response()->json(['message' => "Something went wrong"], 400);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+
+        if (auth()->attempt($data)) {
+            $token = auth()->user()->createToken('LaravelAuthApp')->plainTextToken;
+            return response()->json([
+                'token' => $token,
+                'user' => auth()->user()
+            ], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+//        try {
+//            $user= User::where('email', $request->email)->first();
+//            // print_r($data);
+//            if (!$user || !Hash::check($request->password, $user->password)) {
+//                return response([
+//                    'message' => ['These credentials do not match our records.']
+//                ], 404);
+//            }
+//
+//            $token = $user->createToken('my-app-token')->plainTextToken;
+//
+//            $response = [
+//                'user' => $user,
+//                'token' => $token
+//            ];
+//
+//            return response($response, 201);
+//        } catch (\Exception $e) {
+//            Log::error($e->getMessage());
+//            return response()->json(['message' => 'Something went wrong'], 400);
+//        }
+
+    }
+
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::resource('registration', UserController::class);
+Route::post('registration', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    $user = auth()->user();
+    return response()->json([
+        'message' => 'ok',
+        'user' => $user
+    ], 200);
 });
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
