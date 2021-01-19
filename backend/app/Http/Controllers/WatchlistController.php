0b25cc68891f82b2a46b2ad8f74f918a3f3885ec
belbanas <belbanas@gmail.com>
2021-01-19 14:45:00 +0100

    add watchlist add function

--- backend/app/Http/Controllers/WatchlistController.php
+++ backend/app/Http/Controllers/WatchlistController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Watchlist;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class WatchlistController extends Controller
 {
     //
+    public function addMovie(Request $request)
+    {
+        try {
+            $watchlist = new Watchlist();
+            $watchlist->imdb_id = $request->imdb_id;
+            $watchlist->user_id = auth()->user()->id;
+            $watchlist->save();
+
+            return response()->json([
+                'watchlist' => $watchlist
+            ]);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return response()->json(["message:" => "Something went wrong."], 400);
+        }
+    }
+
+    public function showMovies(Request $request)
+    {
+
+    }
 }
