b89078f68c8474026e6f2897071e9c4049ee053e
sector032 <szantodavid032@gmail.com>
2021-03-10 10:00:00 +0100

    curl fixes

--- backend/app/Http/Controllers/ApiController.php
+++ backend/app/Http/Controllers/ApiController.php
@@ -16,9 +16,9 @@ class ApiController extends Controller
             $url = "http://www.omdbapi.com/?apikey=530b6ee1&s=" . $value . "&page=" . $page;
             curl_setopt($curl, CURLOPT_URL, $url);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            $result = curl_exec($curl);
+            $result = (string)curl_exec($curl);
             curl_close($curl);
-            return response()->json($result,200);
+            return response()->json(json_decode($result),200);
         } catch (\Exception $e) {
             Log::error($e->getMessage());
             return response()->json(["message" => $e->getMessage()], 400);
@@ -33,9 +33,9 @@ class ApiController extends Controller
             $url = 'http://www.omdbapi.com/?apikey=530b6ee1&i=' . $imdbid;
             curl_setopt($curl, CURLOPT_URL, $url);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            $result = curl_exec($curl);
+            $result = (string)curl_exec($curl);
             curl_close($curl);
-            return response()->json($result,200);
+            return response()->json(json_decode($result),200);
         } catch (\Exception $e) {
             Log::error($e->getMessage());
             return response()->json(["message" => $e->getMessage()], 400);
